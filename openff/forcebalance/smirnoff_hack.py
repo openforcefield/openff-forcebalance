6c8f426fba561df6c98e21434561372d1245604a
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-01-12 15:50:21 -0600

    Rework how SMIRNOFF caching is enabled

--- openff/forcebalance/smirnoff_hack.py
+++ openff/forcebalance/smirnoff_hack.py
@@ -1,18 +1,13 @@
 """Cache results of some toolkit functions."""
+import copy
 import os
 
+from openff.toolkit.typing.chemistry.environment import ChemicalEnvironment
 from openff.toolkit.utils.toolkits import (
     AmberToolsToolkitWrapper,
     OpenEyeToolkitWrapper,
     RDKitToolkitWrapper,
 )
-
-# Add a mechanism for disabling SMIRNOFF hack entirely (turned on by default)
-_SHOULD_CACHE = os.environ.get("ENABLE_FB_SMIRNOFF_CACHING", "true").lower() in [
-    "true",
-    "yes",
-    "1",
-]
 
 
 def hash_molecule(molecule):
@@ -37,72 +32,79 @@ def hash_molecule_args_and_kwargs(molecule, *args, **kwargs):
     return hash((hash_molecule(molecule), *arguments, *keywords))
 
 
-if _SHOULD_CACHE:
-    # Commented out because it is printed even for non-SMIRNOFF calculations.
-    # print(
-    #     "SMIRNOFF functions will be replaced with cached versions to improve their "
-    #     "performance."
-    # )
+# Add a mechanism for disabling SMIRNOFF hack entirely (turned on by default)
+if os.environ.get("ENABLE_FB_SMIRNOFF_CACHING", "true").lower() in ["true", "1", "yes"]:
 
-    # time based on total 540s evaluation
-    # cache for OE find_smarts_matches (save 300+ s)
-    oe_original_find_smarts_matches = OpenEyeToolkitWrapper.find_smarts_matches
-    OE_TOOLKIT_CACHE_find_smarts_matches = {}
+    def save_original(obj, method):
+        if not hasattr(obj, f"_original_{method}"):
+            setattr(obj, f"_original_{method}", copy.deepcopy(getattr(obj, method)))
+
+    save_original(OpenEyeToolkitWrapper, "find_smarts_matches")
+    save_original(RDKitToolkitWrapper, "find_smarts_matches")
+    save_original(ChemicalEnvironment, "validate")
+    save_original(OpenEyeToolkitWrapper, "assign_partial_charges")
+    save_original(AmberToolsToolkitWrapper, "assign_partial_charges")
+    save_original(OpenEyeToolkitWrapper, "generate_conformers")
+    save_original(RDKitToolkitWrapper, "generate_conformers")
+
+    (
+        OE_TOOLKIT_CACHE_find_smarts_matches,
+        RDK_TOOLKIT_CACHE_find_smarts_matches,
+        TOOLKIT_CACHE_ChemicalEnvironment_validate,
+        OE_TOOLKIT_CACHE_assign_partial_charges,
+        AT_TOOLKIT_CACHE_assign_partial_charges,
+        OE_TOOLKIT_CACHE_molecule_conformers,
+        RDK_TOOLKIT_CACHE_molecule_conformers,
+    ) = (
+        dict(),
+        dict(),
+        dict(),
+        dict(),
+        dict(),
+        dict(),
+        dict(),
+    )
 
     def oe_cached_find_smarts_matches(self, molecule, *args, **kwargs):
         cache_key = hash_molecule_args_and_kwargs(molecule, args, kwargs)
         if cache_key not in OE_TOOLKIT_CACHE_find_smarts_matches:
             OE_TOOLKIT_CACHE_find_smarts_matches[
                 cache_key
-            ] = oe_original_find_smarts_matches(self, molecule, *args, **kwargs)
+            ] = OpenEyeToolkitWrapper._original_find_smarts_matches(
+                self, molecule, *args, **kwargs
+            )
         return OE_TOOLKIT_CACHE_find_smarts_matches[cache_key]
-
-    # replace the original function with new one
-    OpenEyeToolkitWrapper.find_smarts_matches = oe_cached_find_smarts_matches
-
-    # cache for RDK find_smarts_matches
-    rdk_original_find_smarts_matches = RDKitToolkitWrapper.find_smarts_matches
-    RDK_TOOLKIT_CACHE_find_smarts_matches = {}
 
     def rdk_cached_find_smarts_matches(self, molecule, *args, **kwargs):
         cache_key = hash_molecule_args_and_kwargs(molecule, args, kwargs)
         if cache_key not in RDK_TOOLKIT_CACHE_find_smarts_matches:
             RDK_TOOLKIT_CACHE_find_smarts_matches[
                 cache_key
-            ] = rdk_original_find_smarts_matches(self, molecule, *args, **kwargs)
+            ] = RDKitToolkitWrapper._original_find_smarts_matches(
+                self, molecule, *args, **kwargs
+            )
         return RDK_TOOLKIT_CACHE_find_smarts_matches[cache_key]
-
-    # replace the original function with new one
-    RDKitToolkitWrapper.find_smarts_matches = rdk_cached_find_smarts_matches
-
-    # cache for the validate function (save 94s)
-    from openff.toolkit.typing.chemistry.environment import ChemicalEnvironment
-
-    original_validate = ChemicalEnvironment.validate
-    TOOLKIT_CACHE_ChemicalEnvironment_validate = {}
 
     def cached_validate(
         smirks, validate_valence_type=True, toolkit_registry=OpenEyeToolkitWrapper
     ):
         cache_key = hash((smirks, validate_valence_type, toolkit_registry))
         if cache_key not in TOOLKIT_CACHE_ChemicalEnvironment_validate:
-            TOOLKIT_CACHE_ChemicalEnvironment_validate[cache_key] = original_validate(
+            TOOLKIT_CACHE_ChemicalEnvironment_validate[
+                cache_key
+            ] = ChemicalEnvironment._original_validate(
                 smirks,
                 validate_valence_type=validate_valence_type,
                 toolkit_registry=toolkit_registry,
             )
         return TOOLKIT_CACHE_ChemicalEnvironment_validate[cache_key]
 
-    ChemicalEnvironment.validate = cached_validate
-
-    # Cache for OETK assign_partial_charges
-    oe_original_assign_partial_charges = OpenEyeToolkitWrapper.assign_partial_charges
-    OE_TOOLKIT_CACHE_assign_partial_charges = {}
-
     def oe_cached_assign_partial_charges(self, molecule, *args, **kwargs):
         cache_key = hash_molecule_args_and_kwargs(molecule, args, kwargs)
         if cache_key not in OE_TOOLKIT_CACHE_assign_partial_charges:
-            oe_original_assign_partial_charges(self, molecule, *args, **kwargs)
+            OpenEyeToolkitWrapper._original_assign_partial_charges(
+                self, molecule, *args, **kwargs
+            )
             OE_TOOLKIT_CACHE_assign_partial_charges[
                 cache_key
             ] = molecule.partial_charges
@@ -110,18 +112,13 @@ if _SHOULD_CACHE:
             molecule.partial_charges = OE_TOOLKIT_CACHE_assign_partial_charges[
                 cache_key
             ]
-        return
-
-    OpenEyeToolkitWrapper.assign_partial_charges = oe_cached_assign_partial_charges
-
-    # Cache for AmberTools assign_partial_charges
-    at_original_assign_partial_charges = AmberToolsToolkitWrapper.assign_partial_charges
-    AT_TOOLKIT_CACHE_assign_partial_charges = {}
 
     def at_cached_assign_partial_charges(self, molecule, *args, **kwargs):
         cache_key = hash_molecule_args_and_kwargs(molecule, args, kwargs)
         if cache_key not in AT_TOOLKIT_CACHE_assign_partial_charges:
-            at_original_assign_partial_charges(self, molecule, *args, **kwargs)
+            AmberToolsToolkitWrapper._original_assign_partial_charges(
+                self, molecule, *args, **kwargs
+            )
             AT_TOOLKIT_CACHE_assign_partial_charges[
                 cache_key
             ] = molecule.partial_charges
@@ -129,32 +126,29 @@ if _SHOULD_CACHE:
             molecule.partial_charges = AT_TOOLKIT_CACHE_assign_partial_charges[
                 cache_key
             ]
-        return
-
-    AmberToolsToolkitWrapper.assign_partial_charges = at_cached_assign_partial_charges
-
-    # cache the OE generate_conformers function (save 15s)
-    OE_TOOLKIT_CACHE_molecule_conformers = {}
-    oe_original_generate_conformers = OpenEyeToolkitWrapper.generate_conformers
 
     def oe_cached_generate_conformers(self, molecule, *args, **kwargs):
         cache_key = hash_molecule_args_and_kwargs(molecule, args, kwargs)
         if cache_key not in OE_TOOLKIT_CACHE_molecule_conformers:
-            oe_original_generate_conformers(self, molecule, *args, **kwargs)
+            OpenEyeToolkitWrapper._original_generate_conformers(
+                self, molecule, *args, **kwargs
+            )
             OE_TOOLKIT_CACHE_molecule_conformers[cache_key] = molecule._conformers
         molecule._conformers = OE_TOOLKIT_CACHE_molecule_conformers[cache_key]
-
-    OpenEyeToolkitWrapper.generate_conformers = oe_cached_generate_conformers
-
-    # cache the RDKit generate_conformers function
-    RDK_TOOLKIT_CACHE_molecule_conformers = {}
-    rdk_original_generate_conformers = RDKitToolkitWrapper.generate_conformers
 
     def rdk_cached_generate_conformers(self, molecule, *args, **kwargs):
         cache_key = hash_molecule_args_and_kwargs(molecule, args, kwargs)
         if cache_key not in RDK_TOOLKIT_CACHE_molecule_conformers:
-            rdk_original_generate_conformers(self, molecule, *args, **kwargs)
+            RDKitToolkitWrapper._original_generate_conformers(
+                self, molecule, *args, **kwargs
+            )
             RDK_TOOLKIT_CACHE_molecule_conformers[cache_key] = molecule._conformers
         molecule._conformers = RDK_TOOLKIT_CACHE_molecule_conformers[cache_key]
 
+    OpenEyeToolkitWrapper.find_smarts_matches = oe_cached_find_smarts_matches
+    RDKitToolkitWrapper.find_smarts_matches = rdk_cached_find_smarts_matches
+    ChemicalEnvironment.validate = cached_validate
+    OpenEyeToolkitWrapper.assign_partial_charges = oe_cached_assign_partial_charges
+    AmberToolsToolkitWrapper.assign_partial_charges = at_cached_assign_partial_charges
+    OpenEyeToolkitWrapper.generate_conformers = oe_cached_generate_conformers
     RDKitToolkitWrapper.generate_conformers = rdk_cached_generate_conformers
--- openff/forcebalance/tests/unit_tests/test_smirnoff_hack.py
+++ openff/forcebalance/tests/unit_tests/test_smirnoff_hack.py
@@ -1,11 +1,25 @@
+import inspect
+import os
+import sys
+from unittest import mock
+
 import pytest
 from openff.utilities.testing import skip_if_missing
 
 
-@skip_if_missing("openff.toolkit")
-def test_smirnoff_hack():
-    """Test that using smirnoff_hack.py does not break basic toolkit functionality."""
-    from openff.toolkit import ForceField, Molecule
+def uses_hack(func):
+    return inspect.getsourcefile(func).endswith("openff/forcebalance/smirnoff_hack.py")
+
+
+# Because monkeypatching is a _side effect of importing `smirnoff_hack`_, it is
+# important that these tests be run in this order; flipping between turning the
+# hack on and off is not supported, but could be implemented with an else
+# block in the main logic in `smirnoff_hack.py`.
+def test_disable_smirnoff_hack(monkeypatch):
+    """Test that SMIRNOFF caching can be turned off."""
+    monkeypatch.setitem(os.environ, "ENABLE_FB_SMIRNOFF_CACHING", "false")
+    assert os.environ["ENABLE_FB_SMIRNOFF_CACHING"] == "false"
+
     from openff.toolkit.utils.toolkits import (
         AmberToolsToolkitWrapper,
         RDKitToolkitWrapper,
@@ -14,13 +28,49 @@ def test_smirnoff_hack():
 
     from openff.forcebalance import smirnoff_hack
 
-    topology = Molecule.from_smiles("CCO").to_topology()
-    parsley = ForceField("openff-1.0.0.offxml")
-    toolkit_registry = ToolkitRegistry(
+    registry = ToolkitRegistry(
         toolkit_precedence=[RDKitToolkitWrapper, AmberToolsToolkitWrapper]
     )
 
-    parsley.create_openmm_system(topology, toolkit_registry=toolkit_registry)
+    assert not uses_hack(registry.registered_toolkits[0].find_smarts_matches)
+    assert not uses_hack(registry.registered_toolkits[0].generate_conformers)
+    assert not uses_hack(registry.registered_toolkits[1].assign_partial_charges)
+
+    # Because the hack is turned on or off as a side effect of importing, we need to
+    # "un-import" it; simply flipping the environment variable will not re-trigger
+    # execution of code in a module that's already loaded, mocked or not.
+    sys.modules.pop("openff.forcebalance.smirnoff_hack")
+    sys.modules.pop("openff.forcebalance")
+
+
+@skip_if_missing("openff.toolkit")
+def test_smirnoff_hack_basic():
+    """Test that using smirnoff_hack.py does not break basic toolkit functionality."""
+
+    from openff.toolkit import ForceField, Molecule
+    from openff.toolkit.typing.chemistry.environment import ChemicalEnvironment
+    from openff.toolkit.utils.toolkits import (
+        AmberToolsToolkitWrapper,
+        RDKitToolkitWrapper,
+        ToolkitRegistry,
+    )
+
+    from openff.forcebalance import smirnoff_hack
+
+    assert os.environ.get("ENABLE_FB_SMIRNOFF_CACHING") == None
+
+    topology = Molecule.from_smiles("CCO").to_topology()
+    parsley = ForceField("openff-1.0.0.offxml")
+    registry = ToolkitRegistry(
+        toolkit_precedence=[RDKitToolkitWrapper, AmberToolsToolkitWrapper]
+    )
+
+    parsley.create_openmm_system(topology, toolkit_registry=registry)
+
+    assert uses_hack(registry.registered_toolkits[0].find_smarts_matches)
+    assert uses_hack(registry.registered_toolkits[0].generate_conformers)
+    assert uses_hack(registry.registered_toolkits[1].assign_partial_charges)
+    assert uses_hack(ChemicalEnvironment.validate)
 
 
 # TODO: Test that smirnoff_hack.py improves performance
