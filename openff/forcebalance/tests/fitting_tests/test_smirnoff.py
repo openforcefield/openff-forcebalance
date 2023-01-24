96b7dd2daab2afadea4bdf835680e74bcbcbd92c
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-01-24 10:33:33 -0600

    Move vibration fitting case

--- openff/forcebalance/tests/fitting_tests/test_smirnoff.py
+++ openff/forcebalance/tests/fitting_tests/test_smirnoff.py
@@ -4,3 +4,8 @@ from openff.forcebalance.tests.fitting_tests import fit, fitting_cd
 @fitting_cd("optgeo")
 def test_opgeo():
     fit()
+
+
+@fitting_cd("vibration")
+def test_vibration():
+    fit()
