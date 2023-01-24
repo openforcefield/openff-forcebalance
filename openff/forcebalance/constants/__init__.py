60ce750b0066c3ff72d6f7be8dbd8a6e44873817
Matthew W. Thompson <mattwthompson@protonmail.com>
2023-01-24 11:22:11 -0600

    Move atomic radii out of molecule module

---
+++ openff/forcebalance/constants/__init__.py
@@ -0,0 +1,2 @@
+from openff.forcebalance.constants.atomic_numbers import NUMBERS
+from openff.forcebalance.constants.radii import RADII
--- openff/forcebalance/molecule.py
+++ openff/forcebalance/molecule.py
@@ -253,112 +253,6 @@ else:
 
 module_name = __name__.replace(".molecule", "")
 
-# Covalent radii from Cordero et al. 'Covalent radii revisited' Dalton Transactions 2008, 2832-2838.
-RADII: Dict[int, float] = {
-    index + 1: value
-    for index, value in enumerate(
-        [
-            0.31,
-            0.28,  # H and He
-            1.28,
-            0.96,
-            0.84,
-            0.76,
-            0.71,
-            0.66,
-            0.57,
-            0.58,  # First row elements
-            0.00,
-            1.41,
-            1.21,
-            1.11,
-            1.07,
-            1.05,
-            1.02,
-            1.06,  # Second row elements
-            # 1.66, 1.41, 1.21, 1.11, 1.07, 1.05, 1.02, 1.06, # Second row elements
-            2.03,
-            1.76,
-            1.70,
-            1.60,
-            1.53,
-            1.39,
-            1.61,
-            1.52,
-            1.50,
-            1.24,
-            1.32,
-            1.22,
-            1.22,
-            1.20,
-            1.19,
-            1.20,
-            1.20,
-            1.16,  # Third row elements, K through Kr
-            2.20,
-            1.95,
-            1.90,
-            1.75,
-            1.64,
-            1.54,
-            1.47,
-            1.46,
-            1.42,
-            1.39,
-            1.45,
-            1.44,
-            1.42,
-            1.39,
-            1.39,
-            1.38,
-            1.39,
-            1.40,  # Fourth row elements, Rb through Xe
-            2.44,
-            2.15,
-            2.07,
-            2.04,
-            2.03,
-            2.01,
-            1.99,
-            1.98,
-            1.98,
-            1.96,
-            1.94,
-            1.92,
-            1.92,
-            1.89,
-            1.90,
-            1.87,  # Fifth row elements, s and f blocks
-            1.87,
-            1.75,
-            1.70,
-            1.62,
-            1.51,
-            1.44,
-            1.41,
-            1.36,
-            1.36,
-            1.32,
-            1.45,
-            1.46,
-            1.48,
-            1.40,
-            1.50,
-            1.50,  # Fifth row elements, d and p blocks
-            2.60,
-            2.21,
-            2.15,
-            2.06,
-            2.00,
-            1.96,
-            1.90,
-            1.87,
-            1.80,
-            1.69,
-        ]
-    )
-}
-
 
 # Dictionary of atomic masses ; also serves as the list of elements (periodic table)
 #
@@ -2003,7 +1897,7 @@ class Molecule:
     def center_of_mass(self):
         from openff.units.elements import MASSES, SYMBOLS
 
-        NUMBERS = {v: k for k, v in SYMBOLS.items()}
+        from openff.forcebalance.constants import NUMBERSS
 
         masses = [MASSES[NUMBERS[element]] for element in self.elem]
         total_mass = sum(masses)
@@ -2345,12 +2239,10 @@ class Molecule:
         # Create an atom-wise list of covalent radii.
         # Molecule object can have its own set of radii that overrides the global ones
 
-        from openff.units.elements import SYMBOLS
-
-        NUMBERS = {v: k for k, v in SYMBOLS.items()}
-
         def get_radii(element: str) -> float:
             """Given an element abbreviation, look up the radii."""
+            from openff.forcebalance.constants import NUMBERS, RADII
+
             return RADII.get(NUMBERS.get(element), 0.0)
 
         radii = np.array(
